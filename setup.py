ec1f26f4523cff8645c4ea31fc1fe73dac90ba61
theroggy <pieter.roggemans@gmail.com>
2020-05-25 18:38:48 +0200

    Add requirement on geopandas 0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geofile_ops", 
-    version="0.0.1",
+    version="0.0.4",
     author="Pieter Roggemans",
     author_email="pieter.roggemans@gmail.com",
     description="Package to do spatial operations on data in a geofile.",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/theroggy/geofile_ops",
     packages=setuptools.find_packages(),
+    install_requires=["geopandas>=0.7"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
