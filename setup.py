fab32d687a1ee7984ab81be6c7ec2322a71034b9
theroggy <pieter.roggemans@gmail.com>
2020-06-14 00:24:11 +0200

    Update version to 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="geofileops", 
-    version="0.0.2",
+    version="0.0.3",
     author="Pieter Roggemans",
     author_email="pieter.roggemans@gmail.com",
     description="Package to do spatial operations on geo files.",
