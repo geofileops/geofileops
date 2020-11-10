9a48bbdfb56bdc88c2c39960ce42aaf9b529e6cf
theroggy <pieter.roggemans@gmail.com>
2020-11-10 14:21:11 +0100

    Add inline documentation where missing.

--- geofileops/geofile.py
+++ geofileops/geofile.py
@@ -35,9 +35,17 @@ gdal.UseExceptions()        # Enable exceptions
 # The real work
 #-------------------------------------------------------------
 
-def buildOGRVrt(
+'''
+def _buildOGRVrt(
         output_path: Path,
         input_layer_paths: dict):
+    """
+    Create a vrt file where all layers in the based on .
+
+    Args:
+        output_path (Path): [description]
+        input_layer_paths (dict): [description]
+    """
 
     # Create vrt file
     with open(output_path, "w") as vrt_file:
@@ -51,21 +59,21 @@ def buildOGRVrt(
             vrt_file.write(f'  </OGRVRTLayer>\n')
 
             # layer index
-            '''
+            """
             vrt_file.write(f'  <OGRVRTLayer name="rtree_{input_layer}_geometry">\n')
             vrt_file.write(f'    <SrcDataSource>{input_layer_paths[input_layer]["path"]}</SrcDataSource>\n')
             vrt_file.write(f'    <SrcLayer>rtree_{input_layer}_geom</SrcLayer>\n')
             vrt_file.write(f'  </OGRVRTLayer>\n')
-            '''
+            """
 
         vrt_file.write(f'</OGRVRTDataSource>\n')
-        
+'''
+
 def listlayers(
         path: Union[str, 'os.PathLike[Any]'],
         verbose: bool = False) -> List[str]:
-
     """
-    Get the layers in a geofile.
+    Get the list of layers in a geofile.
 
     Args:
         path (PathLike): path to the file to get info about
@@ -165,7 +173,7 @@ def get_only_layer(path: Union[str, 'os.PathLike[Any]']) -> str:
     If the file contains multiple layers, an exception is thrown.
 
     Args:
-        path (PathLike): the file
+        path (PathLike): the file.
 
     Raises:
         Exception: [description]
@@ -183,13 +191,39 @@ def get_only_layer(path: Union[str, 'os.PathLike[Any]']) -> str:
     else:
         raise Exception(f"Error: More than 1 layer found in {path}: {layers}")
 
-def get_default_layer(path: Union[str, 'os.PathLike[Any]']):
+def get_default_layer(path: Union[str, 'os.PathLike[Any]']) -> str:
+    """
+    Get the default layer name to be used for a layer in this file.
+
+    This is the stem of the filepath.
+
+    Args:
+        path (Union[str,): The path to the file.
+
+    Returns:
+        str: The default layer name
+    """
     return Path(path).stem
 
 def has_spatial_index(
         path: Union[str, 'os.PathLike[Any]'],
         layer: str = None,
         geometrycolumn: str = None) -> bool:
+    """
+    Check if the layer/column has a spatial index.
+
+    Args:
+        path (PathLike): The file path.
+        layer (str, optional): The layer. Defaults to None.
+        geometrycolumn (str, optional): The geometry column to check. 
+            Defaults to None.
+
+    Raises:
+        Exception: [description]
+
+    Returns:
+        bool: True if the spatial column exists.
+    """
     # Init
     path_p = Path(path)
 
@@ -213,6 +247,14 @@ def has_spatial_index(
 def create_spatial_index(
         path: Union[str, 'os.PathLike[Any]'],
         layer: str = None):
+    """
+    Create a spatial index on the layer specified.
+
+    Args:
+        path (PathLike): The file path.
+        layer (str, optional): The layer. If not specified, and there is only 
+            one layer in the file, this layer is used. Otherwise exception.
+    """
     # Init
     path_p = Path(path)
     layerinfo = getlayerinfo(path_p, layer)
@@ -230,6 +272,14 @@ def create_spatial_index(
 def remove_spatial_index(
         path: Union[str, 'os.PathLike[Any]'],
         layer: str = None):
+    """
+    Remove the spatial index from the layer specified.
+
+    Args:
+        path (PathLike): The file path.
+        layer (str, optional): The layer. If not specified, and there is only 
+            one layer in the file, this layer is used. Otherwise exception.
+    """
     # Init
     path_p = Path(path)
     layerinfo = getlayerinfo(path_p, layer)
@@ -250,8 +300,18 @@ def remove_spatial_index(
 
 def rename_layer(
         path: Union[str, 'os.PathLike[Any]'],
-        layer: Optional[str],
-        new_layer: str):
+        new_layer: str,
+        layer: Optional[str] = None):
+    """
+    Rename the layer specified.
+
+    Args:
+        path (PathLike): The file path.
+        layer (Optional[str]): The layer name. If not specified, and there is only 
+            one layer in the file, this layer is used. Otherwise exception.
+        new_layer (str): The new layer name. If not specified, and there is only 
+            one layer in the file, this layer is used. Otherwise exception.
+    """
     # Check input parameters
     path_p = Path(path)
     if layer is None:
@@ -439,7 +499,7 @@ def read_file_sql(
         sql_stmt (str): sql statement to use
         sql_dialect (str, optional): Sql dialect used. Defaults to 'SQLITE'.
         layer (str, optional): The layer to read. If no layer is specified, 
-            reads the only layer in the file or throws error.
+            reads the only layer in the file or throws an Exception.
         ignore_geometry (bool, optional): True not to read/return the geomatry. 
             Defaults to False.
 
@@ -476,6 +536,22 @@ def to_file(
         index: bool = True):
     """
     Writes a pandas dataframe to file. The fileformat is detected based on the filepath extension.
+
+    Args:
+        gdf (gpd.GeoDataFrame): The GeoDataFrame to export to file.
+        path (Union[str,): The file path to write to.
+        layer (str, optional): The layer to read. If no layer is specified, 
+            reads the only layer in the file or throws an Exception.
+        append (bool, optional): True to append to the file. Defaults to False.
+        append_timeout_s (int, optional): The maximum timeout to wait when the 
+            output file is already being written to by another process. 
+            Defaults to 600.
+        index (bool, optional): True to write the pandas index to the file as 
+            well. Defaults to True.
+
+    Raises:
+        Exception: [description]
+        Exception: [description]
     """
     # TODO: think about if possible/how to support adding optional parameter and pass them to next 
     # function, example encoding, float_format,...
@@ -562,18 +638,40 @@ def to_file(
             time.sleep(1)
         
 def get_crs(path: Union[str, 'os.PathLike[Any]']) -> pyproj.CRS:
+    """
+    Get the CRS (projection) of the file
+
+    Args:
+        path (PathLike): Path to the file.
+
+    Returns:
+        pyproj.CRS: The projection of the file
+    """
+    # TODO: seems like support for multiple layers in the file isn't here yet??? 
     with fiona.open(str(path), 'r') as geofile:
         return pyproj.CRS(geofile.crs)
 
 def is_geofile(path: Union[str, 'os.PathLike[Any]']) -> bool:
     """
     Determines based on the filepath if this is a geofile.
+
+    Args:
+        path (PathLike): The file path.
+
+    Returns:
+        bool: True if it is a geo file.
     """
     return is_geofile_ext(Path(path).suffix)
 
 def is_geofile_ext(file_ext: str) -> bool:
     """
     Determines based on the file extension if this is a geofile.
+
+    Args:
+        file_ext (str): the extension. 
+
+    Returns:
+        bool: True if it is a geofile.
     """
     file_ext_lower = file_ext.lower()
     if file_ext_lower in ('.shp', '.gpkg', '.geojson'):
@@ -581,8 +679,7 @@ def is_geofile_ext(file_ext: str) -> bool:
     else:
         return False
 
-def cmp(
-        path1: Union[str, 'os.PathLike[Any]'], 
+def cmp(path1: Union[str, 'os.PathLike[Any]'], 
         path2: Union[str, 'os.PathLike[Any]']) -> bool:
     """
     Compare if two geofiles are identical. 
@@ -591,8 +688,8 @@ def cmp(
     Eg. for shapefiles, the .shp, .shx and .dbf file must be identical.
 
     Args:
-        path1 (PathLike): path to the first file
-        path2 (PathLike): path to the second file
+        path1 (PathLike): path to the first file.
+        path2 (PathLike): path to the second file.
 
     Returns:
         bool: True if the files are identical
@@ -623,8 +720,8 @@ def copy(
     of multiple files (eg. .shp) all files are copied.
 
     Args:
-        src (PathLike): the file to copy
-        dst (PathLike): the location to copy the file(s) to
+        src (PathLike): the file to copy.
+        dst (PathLike): the location to copy the file(s) to.
     """
     # Check input parameters
     src_p = Path(src)
@@ -709,8 +806,25 @@ def append_to(
         append_timeout_s: int = 600,
         verbose: bool = False):
     """
-    Append is not supported for all filetypes in fiona/geopandas (0.8)... so 
-    workaround via gdal needed.
+    Append src file to the dst file.
+
+    remark: append is not supported for all filetypes in fiona/geopandas (0.8)
+    so workaround via gdal needed.
+
+    Args:
+        src (Union[str,): source file path.
+        dst (Union[str,): destination file path.
+        src_layer (str, optional): source layer. Defaults to None.
+        dst_layer (str, optional): destination layer. Defaults to None.
+        force_output_geometrytype (str, optional): [description]. Defaults to None.
+        create_spatial_index (bool, optional): True to create a spatial index 
+            on the destination file/layer. Defaults to True.
+        append_timeout_s (int, optional): Timeout to use if the output file is
+            being written to by another process already. Defaults to 600.
+        verbose (bool, optional): True to write verbose output. Defaults to False.
+
+    Raises:
+        Exception: [description]
     """
     src_p = Path(src)
     dst_p = Path(dst)
@@ -770,7 +884,14 @@ def _append_to_nolock(
 def convert(
         src: Union[str, 'os.PathLike[Any]'], 
         dst: Union[str, 'os.PathLike[Any]']):
-    
+    """
+    Convert the source file to the destination file. File types will be 
+    detected based on the file extensions.
+
+    Args:
+        src (PathLike): The source file path.
+        dst (PathLike): The destination file path.
+    """
     src_p = Path(src)
     dst_p = Path(dst)
 
@@ -784,12 +905,27 @@ def convert(
 def get_driver(path: Union[str, 'os.PathLike[Any]']) -> str:
     """
     Get the driver to use for the file extension of this filepath.
+
+    Args:
+        path (PathLike): The file path.
+
+    Returns:
+        str: The OGR driver name.
     """
     return get_driver_for_ext(Path(path).suffix)
 
 def get_driver_for_ext(file_ext: str) -> str:
     """
     Get the driver to use for this file extension.
+
+    Args:
+        file_ext (str): The extentension.
+
+    Raises:
+        Exception: [description]
+
+    Returns:
+        str: The OGR driver name.
     """
     file_ext_lower = file_ext.lower()
     if file_ext_lower == '.shp':
