f1623b7642b2df1d767859cc19a1801d8f64bfc8
theroggy <pieter.roggemans@gmail.com>
2020-11-15 14:16:37 +0100

    Order tests alphabetically

--- tests/test_geofileops_gpd.py
+++ tests/test_geofileops_gpd.py
@@ -13,33 +13,6 @@ from geofileops.util import geofileops_gpd
 def get_testdata_dir() -> Path:
     return Path(__file__).resolve().parent / 'data'
 
-def test_convexhull_gpkg(tmpdir):
-    # Select some data from input to output file
-    input_path = get_testdata_dir() / 'parcels.gpkg'
-    output_path = Path(tmpdir) / 'parcels_output.gpkg'
-    basetest_convexhull(input_path, output_path)
-
-def test_convexhull_shp(tmpdir):
-    # Select some data from input to output file
-    input_path = get_testdata_dir() / 'parcels.shp'
-    output_path = Path(tmpdir) / 'parcels_output.shp'
-    basetest_convexhull(input_path, output_path)
-
-def basetest_convexhull(input_path, output_path):
-    layerinfo_orig = geofile.getlayerinfo(input_path)
-    geofileops_gpd.convexhull(
-            input_path=input_path,
-            output_path=output_path)
-
-    # Now check if the output file is correctly created
-    assert output_path.exists() == True
-    layerinfo_select = geofile.getlayerinfo(output_path)
-    assert layerinfo_orig.featurecount == layerinfo_select.featurecount
-    assert len(layerinfo_orig.columns) == len(layerinfo_select.columns)
-
-    output_gdf = geofile.read_file(output_path)
-    assert output_gdf['geometry'][0] is not None
-
 def test_buffer_gpkg(tmpdir):
     # Buffer to test dir
     input_path = get_testdata_dir() / 'parcels.gpkg'
@@ -68,61 +41,31 @@ def basetest_buffer(input_path, output_path):
     output_gdf = geofile.read_file(output_path)
     assert output_gdf['geometry'][0] is not None
 
-def test_simplify_gpkg(tmpdir):
-    # Buffer to test dir
+def test_convexhull_gpkg(tmpdir):
+    # Select some data from input to output file
     input_path = get_testdata_dir() / 'parcels.gpkg'
     output_path = Path(tmpdir) / 'parcels_output.gpkg'
-    basetest_simplify(input_path, output_path)
+    basetest_convexhull(input_path, output_path)
 
-def test_simplify_shp(tmpdir):
-    # Buffer to test dir
+def test_convexhull_shp(tmpdir):
+    # Select some data from input to output file
     input_path = get_testdata_dir() / 'parcels.shp'
     output_path = Path(tmpdir) / 'parcels_output.shp'
-    basetest_simplify(input_path, output_path)
+    basetest_convexhull(input_path, output_path)
 
-def basetest_simplify(input_path, output_path):
+def basetest_convexhull(input_path, output_path):
     layerinfo_orig = geofile.getlayerinfo(input_path)
-    geofileops_gpd.simplify(
+    geofileops_gpd.convexhull(
             input_path=input_path,
-            output_path=output_path,
-            tolerance=5)
+            output_path=output_path)
 
-    # Now check if the tmp file is correctly created
+    # Now check if the output file is correctly created
     assert output_path.exists() == True
-    layerinfo_select = geofile.getlayerinfo(input_path)
+    layerinfo_select = geofile.getlayerinfo(output_path)
     assert layerinfo_orig.featurecount == layerinfo_select.featurecount
     assert len(layerinfo_orig.columns) == len(layerinfo_select.columns)
 
-def test_dissolve_nogroupby_gpkg(tmpdir):
-    # Buffer to test dir
-    input_path = get_testdata_dir() / 'parcels.gpkg'
-    output_path = Path(tmpdir) / 'parcels_output.gpkg'
-    basetest_dissolve_nogroupby(input_path, output_path)
-
-def test_dissolve_nogroupby_shp(tmpdir):
-    # Buffer to test dir
-    input_path = get_testdata_dir() / 'parcels.shp'
-    output_path = Path(tmpdir) / 'parcels_output.shp'
-    basetest_dissolve_nogroupby(input_path, output_path)
-
-def basetest_dissolve_nogroupby(input_path, output_path):
-    geofileops_gpd.dissolve(
-            input_path=input_path,
-            output_path=output_path,
-            explodecollections=True)
-
-    # Now check if the result file is correctly created
-    assert output_path.exists() == True
-    layerinfo_orig = geofile.getlayerinfo(input_path)
-    layerinfo_output = geofile.getlayerinfo(output_path)
-    assert layerinfo_output.featurecount == 21
-    assert len(layerinfo_output.columns) >= 0
-
-    # Now check the contents of the result file
-    input_gdf = geofile.read_file(input_path)
     output_gdf = geofile.read_file(output_path)
-    assert input_gdf.crs == output_gdf.crs
-    assert len(output_gdf) == layerinfo_output.featurecount
     assert output_gdf['geometry'][0] is not None
 
 def test_dissolve_groupby_gpkg(tmpdir):
@@ -157,6 +100,63 @@ def basetest_dissolve_groupby(input_path, output_path):
     assert input_gdf.crs == output_gdf.crs
     assert len(output_gdf) == layerinfo_output.featurecount
     assert output_gdf['geometry'][0] is not None
+
+def test_dissolve_nogroupby_gpkg(tmpdir):
+    # Buffer to test dir
+    input_path = get_testdata_dir() / 'parcels.gpkg'
+    output_path = Path(tmpdir) / 'parcels_output.gpkg'
+    basetest_dissolve_nogroupby(input_path, output_path)
+
+def test_dissolve_nogroupby_shp(tmpdir):
+    # Buffer to test dir
+    input_path = get_testdata_dir() / 'parcels.shp'
+    output_path = Path(tmpdir) / 'parcels_output.shp'
+    basetest_dissolve_nogroupby(input_path, output_path)
+
+def basetest_dissolve_nogroupby(input_path, output_path):
+    geofileops_gpd.dissolve(
+            input_path=input_path,
+            output_path=output_path,
+            explodecollections=True)
+
+    # Now check if the result file is correctly created
+    assert output_path.exists() == True
+    layerinfo_orig = geofile.getlayerinfo(input_path)
+    layerinfo_output = geofile.getlayerinfo(output_path)
+    assert layerinfo_output.featurecount == 21
+    assert len(layerinfo_output.columns) >= 0
+
+    # Now check the contents of the result file
+    input_gdf = geofile.read_file(input_path)
+    output_gdf = geofile.read_file(output_path)
+    assert input_gdf.crs == output_gdf.crs
+    assert len(output_gdf) == layerinfo_output.featurecount
+    assert output_gdf['geometry'][0] is not None
+
+def test_simplify_gpkg(tmpdir):
+    # Buffer to test dir
+    input_path = get_testdata_dir() / 'parcels.gpkg'
+    output_path = Path(tmpdir) / 'parcels_output.gpkg'
+    basetest_simplify(input_path, output_path)
+
+def test_simplify_shp(tmpdir):
+    # Buffer to test dir
+    input_path = get_testdata_dir() / 'parcels.shp'
+    output_path = Path(tmpdir) / 'parcels_output.shp'
+    basetest_simplify(input_path, output_path)
+
+def basetest_simplify(input_path, output_path):
+    layerinfo_orig = geofile.getlayerinfo(input_path)
+    geofileops_gpd.simplify(
+            input_path=input_path,
+            output_path=output_path,
+            tolerance=5)
+
+    # Now check if the tmp file is correctly created
+    assert output_path.exists() == True
+    layerinfo_select = geofile.getlayerinfo(input_path)
+    assert layerinfo_orig.featurecount == layerinfo_select.featurecount
+    assert len(layerinfo_orig.columns) == len(layerinfo_select.columns)
 
 if __name__ == '__main__':
     #Prepare tempdir
