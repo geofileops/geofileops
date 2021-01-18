422c48af62c29ad519c43efef7a174ef970d512e
theroggy <pieter.roggemans@gmail.com>
2021-01-18 15:41:17 +0100

    Tests for simplify_ext

--- tests/test_vector_util.py
+++ tests/test_vector_util.py
@@ -4,6 +4,7 @@ Tests for functionalities in vector_util.
 """
 
 from pathlib import Path
+import shapely.geometry as sh_geom
 import sys
 
 # Add path so the local geofileops packages are found 
@@ -43,8 +44,34 @@ def test_split_tiles():
 
     assert len(result) == len(input_tiles) * 8
 
+def test_simplify_ext():
+    input_path = get_testdata_dir() / 'simplify_onborder_testcase.gpkg'
+    input_gdf = geofile.read_file(input_path)
+
+    # Create grid lines geometry 
+    grid_gdf = vector_util.create_grid(
+            total_bounds=(210431.875-1000, 176640.125-1000, 210431.875+1000, 176640.125+1000),
+            nb_columns=2,
+            nb_rows=2,
+            crs='epsg:31370')
+    grid_coords = [tile.exterior.coords for tile in grid_gdf['geometry']]
+    grid_lines = sh_geom.MultiLineString(grid_coords)
+
+    simplified_gdf = input_gdf.geometry.simplify(1)
+    geofile.to_file(simplified_gdf, r"c:\temp\simplified.gpkg")
+
+    simplified_dist_ext_gdf = input_gdf.geometry.apply(
+            lambda geom: vector_util.simplify_ext(geom, 'ramer–douglas–peucker', tolerance=tolerance, keep_points_on=grid_lines))
+    geofile.to_file(simplified_dist_ext_gdf, r"c:\temp\simplified_dist0.25_ext.gpkg")
+
+    tolerance = 16*0.25*0.25
+    simplified_area_ext_gdf = input_gdf.geometry.apply(
+            lambda geom: vector_util.simplify_ext(geom, 'visvalingam-whyatt', tolerance=tolerance, keep_points_on=grid_lines))
+    geofile.to_file(simplified_area_ext_gdf, f"c:/temp/simplified_area{tolerance}_ext.gpkg")
+
 if __name__ == '__main__':
     #import tempfile
     #tmpdir = tempfile.gettempdir()
-    test_create_grid2()
+    #test_create_grid2()
     #test_split_tiles()
+    test_simplify_ext()
