6271f85fb1f7f49b4a41210b32d7285f21bc3064
theroggy <pieter.roggemans@gmail.com>
2021-01-19 19:31:01 +0100

    version to 0.1.2rc2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.1.2rc1"
+version = "0.1.2rc2"
 with open('package_tag_version.txt', mode='w') as file:
     file.write(version)
 
