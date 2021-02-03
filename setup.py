86b69cf0f169d556b04356818cd44c8e82a62dc0
theroggy <pieter.roggemans@gmail.com>
2021-02-03 09:45:12 +0100

    Update version to 0.2.0a1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version = "0.1.2rc3"
+version = "0.2.0a1"
 with open('package_tag_version.txt', mode='w') as file:
     file.write(version)
 
