6c35d5f0344387cc2e1bc5f04e2acd7499a87a4b
theroggy <pieter.roggemans@gmail.com>
2021-11-26 16:57:03 +0100

    Fix error in simplify when points on geometries should be kept

--- geofileops/util/geometry_util.py
+++ geofileops/util/geometry_util.py
@@ -468,7 +468,7 @@ def simplify_ext(
 
         coords_on_border_idx = []
         if keep_points_on is not None:
-            coords_gdf = gpd.GeoDataFrame(geometry=sh_geom.MultiPoint(coords).geoms)
+            coords_gdf = gpd.GeoDataFrame(geometry=list(sh_geom.MultiPoint(coords).geoms))
             coords_on_border_series = coords_gdf.intersects(keep_points_on)
             coords_on_border_idx = np.array(coords_on_border_series.index[coords_on_border_series]).tolist()
 
