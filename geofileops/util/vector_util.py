4c72d685136fa42655d5a196fb4661d47a829ee7
theroggy <pieter.roggemans@gmail.com>
2022-03-08 23:19:48 +0100

    Add deprecation warning

--- geofileops/util/vector_util.py
+++ geofileops/util/vector_util.py
@@ -1,3 +1,5 @@
+import warnings
 
+warnings.warn("the vector_util module is deprecated, please import grid_util and/or geometry_util directly instead", FutureWarning)
 from .grid_util import *
 from .geometry_util import *
