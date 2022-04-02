94b423d761b4845e4ce1a8f437ed7230597c4e31
theroggy <pieter.roggemans@gmail.com>
2022-04-02 02:55:16 +0200

    Remove geopandas benchmark

--- benchmark/benchmark_geopandas.py
+++
@@ -1,8 +0,0 @@
-
-import benchmarker
-
-if __name__ == "__main__":
-    benchmarker.run_benchmarks(["benchmarks_geopandas"])
-
-    # Only run specific benchmark function(s)
-    #benchmarker.run_benchmarks(["benchmarks_geopandas"], ["buffer"])
