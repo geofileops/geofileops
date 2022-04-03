bf12796ebc514484dcf4a8893d6a21f2ea500523
theroggy <pieter.roggemans@gmail.com>
2022-04-03 22:20:58 +0200

    Add benchmark for join_by_location

--- benchmark/benchmarks/benchmarks_geofileops.py
+++ benchmark/benchmarks/benchmarks_geofileops.py
@@ -166,6 +166,40 @@ def intersect(tmp_dir: Path) -> RunResult:
     output_path.unlink()
     return result
 
+def join_by_location_intersect(tmp_dir: Path) -> RunResult:
+    # Init
+    input1_path = testdata.TestFile.AGRIPRC_2018.get_file(tmp_dir)
+    input2_all_path = testdata.TestFile.COMMUNES.get_file(tmp_dir)
+    input2_path = input2_all_path.parent / f"{input2_all_path.stem}_filtered.gpkg"
+    sql_stmt = f"""
+                SELECT * 
+                    FROM "{{input_layer}}" layer 
+                    WHERE """
+    gfo.select(
+            input_path=input2_all_path,
+            output_path=input2_path,
+            sql_stmt=sql_stmt)
+    
+    # Go!
+    start_time = datetime.now()
+    output_path = tmp_dir / f"{input1_path.stem}_inters_{input2_path.stem}.gpkg"
+    gfo.join_by_location(
+            input1_path=input1_path, 
+            input2_path=input2_path, 
+            output_path=output_path,
+            force=True)
+    result = RunResult(
+            package=_get_package(), 
+            package_version=_get_version(),
+            operation="join_by_location",
+            secs_taken=(datetime.now()-start_time).total_seconds(),
+            operation_descr="join_by_location/intersect between 2 agri parcel layers BEFL (2*~500.000 polygons)",
+            run_details={"nb_cpu": multiprocessing.cpu_count()})
+
+    # Cleanup and return
+    output_path.unlink()
+    return result
+    
 def union(tmp_dir: Path) -> RunResult:
     # Init
     input1_path, input2_path = testdata.get_testdata(tmp_dir)
