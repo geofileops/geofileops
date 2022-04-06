e9d1096f7fe8e7f92a8bae8fc6ef7ad257e1629a
theroggy <pieter.roggemans@gmail.com>
2022-04-06 16:24:05 +0200

    join_by_location: support for spatial relations
    + add benchmark

--- benchmark/benchmarks/benchmarks_geofileops.py
+++ benchmark/benchmarks/benchmarks_geofileops.py
@@ -168,9 +168,12 @@ def intersect(tmp_dir: Path) -> RunResult:
     output_path.unlink()
     return result
 
-def join_by_location_intersect(tmp_dir: Path) -> RunResult:
-    # Init
+def join_by_location_intersects(tmp_dir: Path) -> RunResult:
+    # Init-
     input1_path = testdata.TestFile.AGRIPRC_2018.get_file(tmp_dir)
+    input2_path = testdata.TestFile.AGRIPRC_2019.get_file(tmp_dir)
+
+    '''
     input2_all_path = testdata.TestFile.COMMUNES.get_file(tmp_dir)
     input2_path = input2_all_path.parent / f"{input2_all_path.stem}_filtered.gpkg"
     sql_stmt = f"""
@@ -181,27 +184,30 @@ def join_by_location_intersect(tmp_dir: Path) -> RunResult:
             input_path=input2_all_path,
             output_path=input2_path,
             sql_stmt=sql_stmt)
-    
+    '''
+
     # Go!
     start_time = datetime.now()
-    output_path = tmp_dir / f"{input1_path.stem}_inters_{input2_path.stem}.gpkg"
+    output_path = tmp_dir / f"{input1_path.stem}_join_inters_{input2_path.stem}_{_get_package()}.gpkg"
     gfo.join_by_location(
             input1_path=input1_path, 
             input2_path=input2_path, 
             output_path=output_path,
+            spatial_relations_query="intersects is True",
             force=True)
     result = RunResult(
             package=_get_package(), 
             package_version=_get_version(),
-            operation="join_by_location",
+            operation="join_by_location_intersects",
             secs_taken=(datetime.now()-start_time).total_seconds(),
-            operation_descr="join_by_location/intersect between 2 agri parcel layers BEFL (2*~500.000 polygons)",
+            operation_descr="join_by_location_intersects between 2 agri parcel layers BEFL (2*~500.000 polygons)",
             run_details={"nb_cpu": multiprocessing.cpu_count()})
 
     # Cleanup and return
+    logger.info(f"nb features in result: {gfo.get_layerinfo(output_path).featurecount}")
     output_path.unlink()
     return result
-    
+
 def union(tmp_dir: Path) -> RunResult:
     # Init
     input1_path = testdata.TestFile.AGRIPRC_2018.get_file(tmp_dir)
--- tests/test_geofileops_twolayers.py
+++ tests/test_geofileops_twolayers.py
@@ -6,11 +6,14 @@ Tests for operations that are executed using a sql statement on two layers.
 from pathlib import Path
 import sys
 
+import pytest
+
 # Add path so the local geofileops packages are found 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import geofileops as gfo
 from geofileops import GeometryType, PrimitiveType
 from geofileops.util import io_util
+from geofileops.util import _geoops_sql
 from tests import test_helper
 
 def test_erase(tmpdir):
@@ -266,113 +269,125 @@ def basetest_intersect(
     output_gdf = gfo.read_file(output_path)
     assert output_gdf['geometry'][0] is not None
 
-def test_join_by_location(tmpdir):
-    # Prepare test data + run tests
+def test_prepare_spatial_relations_filter():
+    # Test all existing named relations
+    named_relations = ["equals", "touches", "within", "overlaps", "crosses", 
+            "intersects", "contains", "covers", "coveredby"]
+    for relation in named_relations:
+        query = f"{relation} is True"
+        filter = _geoops_sql._prepare_spatial_relations_filter(query)
+        assert filter is not None and filter != ""
+    
+    # Test extra queries that should work
+    ok_queries = [
+            "intersects is False",
+            "(intersects is False and within is True) and crosses is False"
+            "(((T******** is False)))"]
+    for query in ok_queries:
+        filter = _geoops_sql._prepare_spatial_relations_filter(query)
+        assert filter is not None and filter != ""
+
+    # Test queries that should fail
+    error_queries = [
+            ("Intersects is False", "named relations should be in lowercase"),
+            ("intersects Is False", "is should be in lowercase"),
+            ("intersects is false", "false should be False"),
+            ("intersects = false", "= should be is"),
+            ("(intersects is False", "not all brackets are closed"),
+            ("intersects is False)", "more closing brackets then opened ones"),
+            ("T**T**T* is False", "predicate should be 9 characters, not 8"),
+            ("T**T**T**T is False", "predicate should be 9 characters, not 10"),
+            ("A**T**T** is False", "A is not a valid character in a predicate"),
+            ("'T**T**T**' is False", "predicates should not be quoted"),
+            ("[T**T**T** is False ]", "square brackets are not supported"),]
+    for query, error_reason in error_queries:
+        try:
+            _ = _geoops_sql._prepare_spatial_relations_filter(query)
+            error = False
+        except:
+            error = True
+        assert error is True, error_reason
+
+@pytest.mark.parametrize(
+        "suffix, crs_epsg, spatial_relations_query, discard_nonmatching, min_area_intersect, expected_featurecount", 
+        [   (".gpkg", 31370, "intersects is True", False, None, 48),
+            (".gpkg", 31370, "intersects is True", True, None, 28), 
+            (".gpkg", 31370, "intersects is True", True, 1000, 24),
+            (".gpkg", 31370, "intersects is True", False, 1000, 47),
+            (".gpkg", 4326, "intersects is True", False, None, 48),
+            (".shp", 31370, "intersects is True", False, None, 48),
+            (".gpkg", 31370, "T******** is True or *T******* is True", True, None, 28),
+            (".gpkg", 31370, "intersects is False", True, None, 0),
+            (".gpkg", 31370, "intersects is False", False, None, 46), ])
+def test_join_by_location(
+        tmpdir, 
+        suffix: str,
+        spatial_relations_query: str,
+        crs_epsg: int,
+        discard_nonmatching: bool,
+        min_area_intersect: float,
+        expected_featurecount: int):
+    ### Prepare test data + run tests ###
     tmp_dir = Path(tmpdir)
     tmp_dir.mkdir(parents=True, exist_ok=True)
-    for suffix in test_helper.get_test_suffix_list():
-        for crs_epsg in test_helper.get_test_crs_epsg_list():
-            # If test input file is in wrong format, convert it
-            input1_path = test_helper.prepare_test_file(
-                    input_path=test_helper.TestFiles.polygons_parcels_gpkg,
-                    output_dir=tmp_dir,
-                    suffix=suffix,
-                    crs_epsg=crs_epsg)
+    path=test_helper.TestFiles.polygons_parcels_gpkg
+    input1_path = test_helper.prepare_test_file(path, tmp_dir, suffix, crs_epsg)
+    path = test_helper.TestFiles.polygons_zones_gpkg
+    input2_path = test_helper.prepare_test_file(path, tmp_dir, suffix, crs_epsg)
 
-            # If test input file is in wrong format, convert it
-            input2_path = test_helper.prepare_test_file(
-                    input_path=test_helper.TestFiles.polygons_zones_gpkg,
-                    output_dir=tmp_dir,
-                    suffix=suffix,
-                    crs_epsg=crs_epsg)
-        
-            # Now run test
-            output_path = tmp_dir / f"{input1_path.stem}-output{suffix}"
-            print(f"Run test for suffix {suffix}, crs_epsg {crs_epsg}")
-            basetest_join_by_location(input1_path, input2_path, output_path)
-    
-def basetest_join_by_location(
-        input1_path: Path, 
-        input2_path: Path,
-        output_path: Path):
-        
-    ### Test 1: inner join, intersect
+    ### Test join_by_location ###
+    output_path = tmp_dir / f"{input1_path.stem}-output_{discard_nonmatching}_{min_area_intersect}{suffix}"
     gfo.join_by_location(
             input1_path=input1_path,
             input2_path=input2_path,
             output_path=output_path,
-            discard_nonmatching=True,
+            spatial_relations_query=spatial_relations_query,
+            discard_nonmatching=discard_nonmatching,
+            min_area_intersect=min_area_intersect,
             force=True)
 
-    # Now check if the output file is correctly created
-    assert output_path.exists() == True
+    # If no result expected, the output files shouldn't exist
+    if expected_featurecount == 0:
+        assert output_path.exists() is False
+        return
+
+    # Check if the output file is correctly created
+    assert output_path.exists() is True
     layerinfo_input1 = gfo.get_layerinfo(input1_path)
     layerinfo_input2 = gfo.get_layerinfo(input2_path)
     layerinfo_output = gfo.get_layerinfo(output_path)
-    assert layerinfo_output.featurecount == 28
-    if input1_path.suffix == ".shp":
-        assert (len(layerinfo_input1.columns) + len(layerinfo_input2.columns)) == len(layerinfo_output.columns)
-    else:
-        assert (len(layerinfo_input1.columns) + len(layerinfo_input2.columns) + 1) == len(layerinfo_output.columns)
+    assert layerinfo_output.featurecount == expected_featurecount
+    assert len(layerinfo_output.columns) == (
+            len(layerinfo_input1.columns) + len(layerinfo_input2.columns) + 1)
     assert layerinfo_output.geometrytype == GeometryType.MULTIPOLYGON 
 
     # Now check the contents of the result file
     output_gdf = gfo.read_file(output_path)
     assert output_gdf['geometry'][0] is not None
 
-    ### Test 2: left outer join, intersect
-    gfo.join_by_location(
-            input1_path=input1_path,
-            input2_path=input2_path,
-            output_path=output_path,
-            discard_nonmatching=False,
-            force=True)
-
-    # Now check if the output file is correctly created
-    assert output_path.exists() == True
-    layerinfo_output = gfo.get_layerinfo(output_path)
-    assert layerinfo_output.featurecount == 48
-    if input1_path.suffix == ".shp":
-        assert (len(layerinfo_input1.columns) + len(layerinfo_input2.columns)) == len(layerinfo_output.columns)
-    else:
-        assert (len(layerinfo_input1.columns) + len(layerinfo_input2.columns) + 1) == len(layerinfo_output.columns)
-    assert layerinfo_output.geometrytype == GeometryType.MULTIPOLYGON
-
-    # Now check the contents of the result file
-    output_gdf = gfo.read_file(output_path)
-    assert output_gdf['geometry'][0] is not None
-
-def test_join_nearest(tmpdir):
+@pytest.mark.parametrize("suffix, crs_epsg", [(".gpkg", 31370), (".gpkg", 4384), (".shp", 31370)])
+def test_join_nearest(tmpdir, suffix, crs_epsg):
     # Prepare test data + run tests
     tmp_dir = Path(tmpdir)
     tmp_dir.mkdir(parents=True, exist_ok=True)
-    for suffix in test_helper.get_test_suffix_list():
-        for crs_epsg in test_helper.get_test_crs_epsg_list():
-            # If test input file is in wrong format, convert it
-            input1_path = test_helper.prepare_test_file(
-                    input_path=test_helper.TestFiles.polygons_parcels_gpkg,
-                    output_dir=tmp_dir,
-                    suffix=suffix,
-                    crs_epsg=crs_epsg)
-
-            # If test input file is in wrong format, convert it
-            input2_path = test_helper.prepare_test_file(
-                    input_path=test_helper.TestFiles.polygons_zones_gpkg,
-                    output_dir=tmp_dir,
-                    suffix=suffix,
-                    crs_epsg=crs_epsg)
-        
-            # Now run test
-            output_path = tmp_dir / f"{input1_path.stem}-output{suffix}"
-            print(f"Run test for suffix {suffix}, crs_epsg {crs_epsg}")
-            basetest_join_nearest(input1_path, input2_path, output_path)
     
-def basetest_join_nearest(
-        input1_path: Path, 
-        input2_path: Path,
-        output_path: Path):
-        
-    ### Test 1: inner join, intersect
+    # If test input file is in wrong format, convert it
+    input1_path = test_helper.prepare_test_file(
+            input_path=test_helper.TestFiles.polygons_parcels_gpkg,
+            output_dir=tmp_dir,
+            suffix=suffix,
+            crs_epsg=crs_epsg)
+
+    # If test input file is in wrong format, convert it
+    input2_path = test_helper.prepare_test_file(
+            input_path=test_helper.TestFiles.polygons_zones_gpkg,
+            output_dir=tmp_dir,
+            suffix=suffix,
+            crs_epsg=crs_epsg)
+
+    # Now run test
+    output_path = tmp_dir / f"{input1_path.stem}-output{suffix}"
+    print(f"Run test for suffix {suffix}, crs_epsg {crs_epsg}")
     nb_nearest = 2
     gfo.join_nearest(
             input1_path=input1_path,
@@ -474,7 +489,7 @@ def basetest_select_two_layers(
     assert output_gdf['geometry'][0] is not None
 
 def test_split(tmpdir):
-   # Prepare test data + run tests
+    # Prepare test data + run tests
     tmp_dir = Path(tmpdir)
     tmp_dir.mkdir(parents=True, exist_ok=True)
     for suffix in test_helper.get_test_suffix_list():
@@ -655,18 +670,3 @@ def basetest_union_circles(
     # Now check the contents of the result file
     output_gdf = gfo.read_file(output_path)
     assert output_gdf['geometry'][0] is not None
-
-if __name__ == '__main__':
-    # Init
-    tmpdir = test_helper.init_test_for_debug(Path(__file__).stem)
-
-    # Two layer operations
-    test_erase(tmpdir)
-    #test_export_by_distance(tmpdir)
-    #test_export_by_location(tmpdir)
-    #test_intersect(tmpdir)
-    #test_join_by_location(tmpdir)
-    #test_select_two_layers(tmpdir)
-    #test_split(tmpdir)
-    #test_union(tmpdir)
-    
