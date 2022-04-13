6abc0fe99d5c0de7e96d8a5ca27a5ca60466a9dd
theroggy <pieter.roggemans@gmail.com>
2022-04-13 12:12:45 +0200

    apply to buffer and simplify

--- tests/test_geofileops_singlelayer.py
+++ tests/test_geofileops_singlelayer.py
@@ -7,6 +7,7 @@ from importlib import import_module
 from pathlib import Path
 import sys
 
+import pytest
 
 # Add path so the local geofileops packages are found 
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
@@ -15,6 +16,7 @@ from geofileops import fileops
 from geofileops import GeometryType
 from geofileops.util import _io_util
 from tests import test_helper
+from tests.test_helper import TestFiles
 
 # Init gfo module
 current_fileops_module = "geofileops.fileops"
@@ -38,52 +40,49 @@ def get_nb_parallel() -> int:
 def get_batchsize() -> int:
     return 5
 
-def test_buffer_basic(tmpdir):
-    # Init
-    tmp_basedir = Path(tmpdir)
+def get_combinations_to_test() -> list:
+    result = []
+    fileops_modules = ["geofileops.geoops", "geofileops.util._geoops_gpd", "geofileops.util._geoops_sql"]
+    testfiles = [   
+            (TestFiles.polygons_parcels_gpkg, GeometryType.MULTIPOLYGON),
+            (TestFiles.points_gpkg, GeometryType.MULTIPOINT),
+            (TestFiles.linestrings_rows_of_trees_gpkg, GeometryType.MULTILINESTRING) ]
+
+    # Test all combination of fileops_modules, testfiles, crs_epsg 31370 on .shp 
+    for fileops_module in fileops_modules:
+        for testfile_path, testfile_geometrytype in testfiles:
+            result.append((".shp", 31370, fileops_module, testfile_path, testfile_geometrytype))
     
-    test_inputs = []
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.polygons_parcels_gpkg,
-            "geometrytype": GeometryType.MULTIPOLYGON})
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.points_gpkg,
-            "geometrytype": GeometryType.MULTIPOINT})
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.linestrings_rows_of_trees_gpkg,
-            "geometrytype": GeometryType.MULTILINESTRING})
+    # Test all combination of fileops_modules, testfiles, crs_epsgs on .gpkg 
+    for crs_epsg in test_helper.get_test_crs_epsg_list():
+        for fileops_module in fileops_modules:
+            for testfile_path, testfile_geometrytype in testfiles:
+                result.append((".gpkg", crs_epsg, fileops_module, testfile_path, testfile_geometrytype))
+        
+    return result
 
-    # Prepare test data + run tests
-    for fileops_module in ["geofileops.geoops", "geofileops.util._geoops_gpd", "geofileops.util._geoops_sql"]:
-        tmp_dir = tmp_basedir / fileops_module
-        tmp_dir.mkdir(parents=True, exist_ok=True)
-
-        for suffix in test_helper.get_test_suffix_list():
-            # Buffer on not-projected data is weird, so not tested (at the moment)  
-            for crs_epsg in test_helper.get_test_crs_epsg_list():
-                for test_input in test_inputs: 
-                    # If test input file is in wrong format, convert it
-                    input_path = test_helper.prepare_test_file(
-                            input_path=test_input['input_path'],
-                            output_dir=tmp_dir,
-                            suffix=suffix,
-                            crs_epsg=crs_epsg)
-
-                    # Now run test
-                    output_path = tmp_dir / f"{input_path.stem}-output{suffix}"
-                    print(f"Run test for suffix {suffix}, crs_epsg {crs_epsg}, geometrytype {test_input['geometrytype']}")
-                    basetest_buffer_basics(input_path, output_path, test_input['geometrytype'], fileops_module)
-
-def basetest_buffer_basics(
-        input_path: Path, 
-        output_path: Path, 
-        input_geometry_type: GeometryType,
-        fileeops_module: str):
+@pytest.mark.parametrize(
+        "suffix, crs_epsg, fileops_module, input_path, expected_geometrytype", 
+        get_combinations_to_test())
+def test_buffer_basic(tmpdir, suffix, crs_epsg, fileops_module, input_path, expected_geometrytype):
     """ 
     Buffer basics are available both in the gpd and sql implementations. 
     """
-    ### Init ###
-    set_fileops_module(fileeops_module)
+    # Prepare test data
+    tmp_basedir = Path(tmpdir)
+    tmp_dir = tmp_basedir / fileops_module
+    tmp_dir.mkdir(parents=True, exist_ok=True)
+
+    # If test input file is in wrong format, convert it
+    input_path = test_helper.prepare_test_file(
+            input_path=input_path,
+            output_dir=tmp_dir,
+            suffix=suffix,
+            crs_epsg=crs_epsg)
+
+    # Now run test
+    output_path = tmp_dir / f"{input_path.stem}-output{suffix}"
+    set_fileops_module(fileops_module)
     layerinfo_input = fileops.get_layerinfo(input_path)
     assert layerinfo_input.crs is not None
     distance = 1
@@ -145,7 +144,7 @@ def basetest_buffer_basics(
             nb_parallel=get_nb_parallel())
 
     # Now check if the output file is correctly created
-    if input_geometry_type in [GeometryType.MULTIPOINT, GeometryType.MULTILINESTRING]:
+    if expected_geometrytype in [GeometryType.MULTIPOINT, GeometryType.MULTILINESTRING]:
         # A Negative buffer of points or linestrings doesn't give a result.
         assert output_path.exists() == False
     else:    
@@ -176,7 +175,7 @@ def basetest_buffer_basics(
             nb_parallel=get_nb_parallel())
 
     # Now check if the output file is correctly created
-    if input_geometry_type in [GeometryType.MULTIPOINT, GeometryType.MULTILINESTRING]:
+    if expected_geometrytype in [GeometryType.MULTIPOINT, GeometryType.MULTILINESTRING]:
         # A Negative buffer of points or linestrings doesn't give a result.
         assert output_path.exists() == False
     else:    
@@ -250,50 +249,20 @@ def basetest_convexhull(
     output_gdf = fileops.read_file(output_path)
     assert output_gdf['geometry'][0] is not None
 
-def test_simplify_basic(tmpdir):
-
-    test_inputs = []
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.polygons_parcels_gpkg,
-            "geometrytype": GeometryType.MULTIPOLYGON})
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.points_gpkg,
-            "geometrytype": GeometryType.MULTIPOINT})
-    test_inputs.append({
-            "input_path": test_helper.TestFiles.linestrings_rows_of_trees_gpkg,
-            "geometrytype": GeometryType.MULTILINESTRING})
-
-    # Prepare test data + run tests
+@pytest.mark.parametrize(
+        "suffix, crs_epsg, fileops_module, input_path, expected_geometrytype", 
+        get_combinations_to_test())
+def test_simplify_basic(tmpdir, suffix, crs_epsg, fileops_module, input_path, expected_geometrytype):
+    # Prepare test data
     tmp_basedir = Path(tmpdir)
-
-    # In fileops, the sql version is used for basic simplify
-    for fileops_module in ["geofileops.geoops", "geofileops.util._geoops_gpd"]:
-        for suffix in test_helper.get_test_suffix_list():
-            for crs_epsg in test_helper.get_test_crs_epsg_list():
-                tmp_dir = tmp_basedir / f"{fileops_module}_{crs_epsg}"
-                tmp_dir.mkdir(parents=True, exist_ok=True)
-
-                for test_input in test_inputs: 
-                    # If test input file is in wrong format, convert it
-                    input_path = test_helper.prepare_test_file(
-                            input_path=test_input['input_path'],
-                            output_dir=tmp_dir,
-                            suffix=suffix,
-                            crs_epsg=crs_epsg)
-
-                    # Now run test
-                    output_path = tmp_dir / f"{input_path.stem}-output{suffix}"
-                    print(f"Run test for suffix {suffix}, crs_epsg {crs_epsg}, geometrytype {test_input['geometrytype']}")
-                    basetest_simplify_basic(
-                            input_path, output_path, test_input['geometrytype'], fileops_module)
-    
-def basetest_simplify_basic(
-        input_path: Path, 
-        output_path: Path,
-        expected_output_geometrytype: GeometryType,
-        fileops_module: str):
-
-    ### Init ###
+    tmp_dir = tmp_basedir / f"{fileops_module}_{crs_epsg}"
+    tmp_dir.mkdir(parents=True, exist_ok=True)
+    input_path = test_helper.prepare_test_file(
+            input_path=input_path,
+            output_dir=tmp_dir,
+            suffix=suffix,
+            crs_epsg=crs_epsg)
+    output_path = tmp_dir / f"{input_path.stem}-output{suffix}"
     set_fileops_module(fileops_module)
     layerinfo_orig = fileops.get_layerinfo(input_path)
     assert layerinfo_orig.crs is not None
@@ -316,9 +285,7 @@ def basetest_simplify_basic(
     layerinfo_output = fileops.get_layerinfo(output_path)
     assert layerinfo_orig.featurecount == layerinfo_output.featurecount
     assert len(layerinfo_orig.columns) == len(layerinfo_output.columns)
-
-    # Check geometry type
-    assert layerinfo_output.geometrytype == expected_output_geometrytype
+    assert layerinfo_output.geometrytype == expected_geometrytype
 
     # Now check the contents of the result file
     input_gdf = fileops.read_file(input_path)
@@ -326,3 +293,6 @@ def basetest_simplify_basic(
     assert input_gdf.crs == output_gdf.crs
     assert len(output_gdf) == layerinfo_output.featurecount
     assert output_gdf['geometry'][0] is not None
+    #expected_gdf = fileops.read_file(input_path)
+    #expected_gdf.geometry = expected_gdf.geometry.simplify(tolerance=tolerance)
+    #assert_geodataframe_equal(output_gdf, expected_gdf)
