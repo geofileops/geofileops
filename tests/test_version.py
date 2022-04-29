01782a862c39dea3b335bb60b4aca2701ef1032b
Pieter Roggemans <pieter.roggemans@gmail.com>
2022-04-29 20:13:25 +0200

    General improvements to tests (#109)

--- tests/test_version.py
+++ tests/test_version.py
@@ -6,9 +6,10 @@ Test if backwards compatibility for old API still works.
 from pathlib import Path
 import sys
 
-# Add path so the local geofileops packages are found 
+# Add path so the local geofileops packages are found
 sys.path.insert(0, str(Path(__file__).resolve().parent.parent))
 import geofileops as gfo
+
 
 def test_version():
     assert "\n" not in gfo.__version__
