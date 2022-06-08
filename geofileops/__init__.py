a15d1a9a87c2789ff1ed65ec7460f37b5f9cd0ea
Pieter Roggemans <pieter.roggemans@gmail.com>
2022-06-08 15:29:28 +0200

    Add view_angles function for geoseries and geometry (#137)

--- geofileops/__init__.py
+++ geofileops/__init__.py
@@ -3,9 +3,11 @@ from pathlib import Path
 from geofileops.fileops import *
 from geofileops.geoops import *
 
+
 def _get_version():
-    version_path = Path(__file__).resolve().parent / 'version.txt'
-    with open(version_path, mode='r') as file:
+    version_path = Path(__file__).resolve().parent / "version.txt"
+    with open(version_path, mode="r") as file:
         return file.readline()
+
 
 __version__ = _get_version()
