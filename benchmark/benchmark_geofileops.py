eac07eeb51d7bdd9ebe13ac0261e3868ec9aecbc
theroggy <pieter.roggemans@gmail.com>
2023-03-01 14:50:13 +0100

    Revert change to benchmark

--- benchmark/benchmark_geofileops.py
+++ benchmark/benchmark_geofileops.py
@@ -1,7 +1,7 @@
 import benchmarker
 
 if __name__ == "__main__":
-    # benchmarker.run_benchmarks(["benchmarks_geofileops"])
+    benchmarker.run_benchmarks(["benchmarks_geofileops"])
 
     # Only run specific benchmark function(s)
-    benchmarker.run_benchmarks(["benchmarks_geofileops"], ["buffer"])
+    # benchmarker.run_benchmarks(["benchmarks_geofileops"], ["clip"])
