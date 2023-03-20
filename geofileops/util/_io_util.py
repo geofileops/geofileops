f175b3133dddf1b43bdfb0b9710b664d71f1dcd3
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-03-20 15:26:07 +0100

    Add parent_dir param to create_tempdir (#245)

--- geofileops/util/_io_util.py
+++ geofileops/util/_io_util.py
@@ -11,15 +11,23 @@ import tempfile
 from typing import Any, Optional, Tuple, Union
 
 
-def create_tempdir(base_dirname: str) -> Path:
+def create_tempdir(base_dirname: str, parent_dir: Optional[Path] = None) -> Path:
     """
     Creates a new tempdir in the default temp location.
 
     Remark: the temp dir won't be cleaned up automatically!
 
+    Examples:
+
+        - base_dirname="foo" -> /tmp/foo_000001
+        - base_dirname="foo/bar" -> /tmp/foo/bar_000001
+
     Args:
-        base_dirname (str): The name the tempdir will start with. A number will
-            be attended to this to make the directory name unique.
+        base_dirname (str): The name the tempdir will start with. The name will be
+            suffixed with a number to make the directory name unique. If a "/" is part
+            of the base_dirname a subdirectory will be created: e.g. "foo/bar".
+        parent_dir (Path, optional): The dir to create the tempdir in. If None, the
+            system temp dir is used. Defaults to None.
 
     Raises:
         Exception: if it wasn't possible to create the temp dir because there
@@ -29,19 +37,20 @@ def create_tempdir(base_dirname: str) -> Path:
         Path: the path to the temp dir created.
     """
 
-    defaulttempdir = Path(tempfile.gettempdir())
+    if parent_dir is None:
+        parent_dir = Path(tempfile.gettempdir())
 
     for i in range(1, 999999):
         try:
-            tempdir = defaulttempdir / f"{base_dirname}_{i:06d}"
+            tempdir = parent_dir / f"{base_dirname}_{i:06d}"
             tempdir.mkdir(parents=True)
-            return Path(tempdir)
+            return tempdir
         except FileExistsError:
             continue
 
     raise Exception(
         f"Wasn't able to create a temporary dir with basedir: "
-        f"{defaulttempdir / base_dirname}"
+        f"{parent_dir / base_dirname}"
     )
 
 
