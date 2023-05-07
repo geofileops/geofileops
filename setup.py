78403ab793f418441402ca3f093dee6931c40e00
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-05-07 12:45:23 +0200

    Pin geopandas to <0.13 (#288)

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setuptools.setup(
         "cloudpickle",
         "fiona",
         "gdal",
-        "geopandas>=0.11,<1.0",
+        "geopandas>=0.11,<0.13",
         "numpy",
         "pandas",
         "psutil",
