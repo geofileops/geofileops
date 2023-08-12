c601e6c74193f8eb3a8ad7c205f638d87d848f1c
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-08-12 11:13:30 +0200

    Ignore some lines that don't need test coverage

--- geofileops/util/_sqlite_util.py
+++ geofileops/util/_sqlite_util.py
@@ -80,12 +80,12 @@ def create_new_spatialdb(path: Path, crs_epsg: Optional[int] = None):
                 sql = "DROP TRIGGER gpkg_metadata_reference_row_id_value_insert;"
                 try:
                     conn.execute(sql)
-                except Exception:
+                except Exception:  # pragma: no cover
                     pass
                 sql = "DROP TRIGGER gpkg_metadata_reference_row_id_value_update;"
                 try:
                     conn.execute(sql)
-                except Exception:
+                except Exception:  # pragma: no cover
                     pass
 
             elif output_suffix_lower == ".sqlite":
