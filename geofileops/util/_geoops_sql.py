79ac310eb71c93d9300e7a4b37914233bd88d3cf
Pieter Roggemans <pieter.roggemans@gmail.com>
2023-11-20 11:03:29 +0100

    Rollback unnecessary change in PR #434 (#435)

--- geofileops/util/_geoops_sql.py
+++ geofileops/util/_geoops_sql.py
@@ -1973,7 +1973,7 @@ def symmetric_difference(
                 gfo.add_column(
                     erase1_output_path,
                     name=f"{input2_columns_prefix}{column}",
-                    type=input2_info.columns[column].sqlite_type,
+                    type=input2_info.columns[column].gdal_type,
                 )
 
         # Now erase input1 from input2 to another temporary output file
