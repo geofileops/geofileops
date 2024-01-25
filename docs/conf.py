df83e131512e9c07fddc2d798f61dc9446478373
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-01-25 19:14:58 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -91,6 +91,8 @@ html_theme_options = {
     ]
 }
 
+html_sidebars = {"getting_started": [], "user_guide": [], "development": []}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
