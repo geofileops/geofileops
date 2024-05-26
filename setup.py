a50a807054d99fb433dd8670c0d12863cbdbaf8b
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-05-26 20:03:12 +0200

    ENH: Final touches for release 0.9

--- setup.py
+++ setup.py
@@ -39,5 +39,5 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.9",
 )
