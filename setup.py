54e5fa94d5826bccb1c8904be3328c5d5129ab1a
Pieter Roggemans <pieter.roggemans@gmail.com>
2024-07-12 07:17:21 +0200

    Update geopandas dependency to <1.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setuptools.setup(
     install_requires=[
         "cloudpickle",
         "gdal>=3.6,<3.10",
-        "geopandas>=0.12,<1",
+        "geopandas>=0.12,<1.1",
         "numpy",
         "packaging",
         "pandas",
