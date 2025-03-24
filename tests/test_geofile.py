geofileops/geofileops

commit 4135e8c1fdebf61cafd198f95618de31f19f7525

ENH: support GDAL vsi handlers in paths in most general fileops file/layer operations (#669)
Add support for GDAL vsi handlers for most general file operations. They
are not (yet) supported in spatial operations.

Additional changes:
- When input files are not found, a FileNotFoundError is thrown now
rather that a ValueError
- The deprecation of `append_to` also ended up in this PR :-(

tests/test_helper.py
@@ -14,7 +14,9 @@ import shapely.geometry as sh_geom
 import geofileops as gfo
 from geofileops.util import _geofileinfo, _geoseries_util, _io_util, geodataframe_util
 
-_data_dir = Path(__file__).parent.resolve() / "data"
+data_dir = Path(__file__).parent.resolve() / "data"
+data_url = "https://raw.githubusercontent.com/geofileops/geofileops/main/tests/data"
+
 EPSGS = [31370, 4326]
 GRIDSIZE_DEFAULT = 0.0
 SUFFIXES_FILEOPS = [".gpkg", ".shp", ".csv"]
@@ -139,7 +141,7 @@ def get_testfile(
     explodecollections: bool = False,
 ) -> Path:
     # Prepare original filepath.
-    testfile_path = _data_dir / f"{testfile}.gpkg"
+    testfile_path = data_dir / f"{testfile}.gpkg"
     if not testfile_path.exists():
         raise ValueError(f"Invalid testfile type: {testfile}")
 

.pre-commit-config.yaml
@@ -12,6 +12,6 @@ repos:
       - id: ruff
         # args: [ --fix ]
   - repo: https://github.com/pre-commit/mirrors-mypy
-    rev: "v1.8.0"
+    rev: "v1.15.0"
     hooks:
       - id: mypy

environment-dev.yml
@@ -27,7 +27,7 @@ dependencies:
   - pytest
   - pytest-cov
   # linting
-  - mypy
+  - mypy ==1.15.0
   - pandas-stubs
   - pre-commit
   - ruff ==0.11.0

geofileops/util/_geofileinfo.py
@@ -229,25 +229,28 @@ def get_driver(path: Union[str, "os.PathLike[Any]"]) -> str:
     """Get the gdal driver name for the file specified.
 
     Args:
-        path (PathLike): The file path.
+        path (PathLike): The file path. |GDAL_vsi| paths are also supported.
 
     Returns:
         str: The OGR driver name.
-    """
-    path = Path(path)
 
+    .. |GDAL_vsi| raw:: html
+
+        <a href="https://gdal.org/en/stable/user/virtual_file_systems.html" target="_blank">GDAL vsi</a>
+
+    """  # noqa: E501
     # gdal.OpenEx is relatively slow on windows, so for straightforward cases, avoid it.
-    if path.suffix.lower() == ".gpkg":
+    suffix = Path(path).suffix.lower()
+    if suffix == ".gpkg":
         return "GPKG"
-    elif path.suffix.lower() == ".shp":
+    elif suffix == ".shp":
         return "ESRI Shapefile"
 
-    def get_driver_for_path(input_path) -> str:
+    def get_driver_for_path(input_path: Union[str, "os.PathLike[Any]"]) -> str:
         # If there is no suffix, possibly it is only a suffix, so prefix with filename
-        if input_path.suffix == "":
+        local_path = input_path
+        if Path(input_path).suffix == "":
             local_path = f"temp{input_path}"
-        else:
-            local_path = input_path
 
         drivers = GetOutputDriversFor(local_path, is_raster=False)
         if len(drivers) == 1:

geofileops/util/_geoops_gpd.py
@@ -746,7 +746,7 @@ def _apply_geooperation_to_layer(
 
     # Check input parameters...
     if not input_path.exists():
-        raise ValueError(f"{operation_name}: input_path doesn't exist: {input_path}")
+        raise FileNotFoundError(f"{operation_name}: input_path not found: {input_path}")
     if input_path == output_path:
         raise ValueError(f"{operation_name}: output_path must not equal input_path")
     if _io_util.output_exists(path=output_path, remove_if_exists=force):
@@ -838,6 +838,7 @@ def _apply_geooperation_to_layer(
                     gridsize=gridsize,
                     keep_empty_geoms=keep_empty_geoms,
                     preserve_fid=preserve_fid,
+                    create_spatial_index=False,
                     force=force,
                 )
                 future_to_batch_id[future] = batch_id
@@ -940,6 +941,7 @@ def _apply_geooperation(
     gridsize: float = 0.0,
     keep_empty_geoms: bool = False,
     preserve_fid: bool = False,
+    create_spatial_index: bool = False,
     force: bool = False,
 ) -> str:
     # Init
@@ -1041,7 +1043,7 @@ def _apply_geooperation(
         index=False,
         force_output_geometrytype=force_output_geometrytype,
         force_multitype=not explodecollections,
-        create_spatial_index=False,
+        create_spatial_index=create_spatial_index,
     )
 
     message = f"Took {datetime.now() - start_time} for {len(data_gdf)} rows ({where})"
@@ -1123,7 +1125,7 @@ def dissolve(
     if groupby_columns is not None and len(list(groupby_columns)) == 0:
         raise ValueError("groupby_columns=[] is not supported. Use None.")
     if not input_path.exists():
-        raise ValueError(f"input_path doesn't exist: {input_path}")
+        raise FileNotFoundError(f"input_path not found: {input_path}")
     if input_path == output_path:
         raise ValueError("output_path must not equal input_path")
 

geofileops/util/_geoops_sql.py
@@ -558,7 +558,7 @@ def _single_layer_vector_operation(
 
     # Check/clean input parameters...
     if not input_path.exists():
-        raise ValueError(f"{operation_name}: input_path doesn't exist: {input_path}")
+        raise FileNotFoundError(f"{operation_name}: input_path not found: {input_path}")
     if input_path == output_path:
         raise ValueError(f"{operation_name}: output_path must not equal input_path")
     if where_post is not None and where_post == "":
@@ -3640,9 +3640,13 @@ def _validate_params(
         input1_layer (LayerInfo), input2_layer (LayerInfo), output_layer (str)
     """
     if not input1_path.exists():
-        raise ValueError(f"{operation_name}: input1_path doesn't exist: {input1_path}")
+        raise FileNotFoundError(
+            f"{operation_name}: input1_path not found: {input1_path}"
+        )
     if not input2_path.exists():
-        raise ValueError(f"{operation_name}: input2_path doesn't exist: {input2_path}")
+        raise FileNotFoundError(
+            f"{operation_name}: input2_path not found: {input2_path}"
+        )
     if output_path in (input1_path, input2_path):
         raise ValueError(
             f"{operation_name}: output_path must not equal one of input paths"
@@ -4123,7 +4127,7 @@ def dissolve_singlethread(
 
     # Check input params
     if not input_path.exists():
-        raise ValueError(f"input_path doesn't exist: {input_path}")
+        raise FileNotFoundError(f"input_path not found: {input_path}")
     if input_path == output_path:
         raise ValueError("output_path must not equal input_path")
     if where_post is not None and where_post == "":

commit 157e2aafa8974e240ebccac995ed0e2dbf7407c6

BUG: fix issue in listlayers introduced in #669

geofileops/fileops.py
@@ -113,6 +113,8 @@ def listlayers(
     """  # noqa: E501
     path_p = Path(path)
     if path_p.suffix.lower() == ".shp":
+        if not _vsi_exists(path):
+            raise FileNotFoundError(f"File not found: {path}")
         return [path_p.stem]
 
     datasource = None

tests/test_geofile.py
@@ -245,7 +245,7 @@ def test_copy_layer(tmp_path, testfile, dimensions, suffix_input, suffix_output)
         assert src_layerinfo.geometrytypename == dst_layerinfo.geometrytypename
 
 
-def test_copy_layer_add_layer(tmp_path):
+def test_copy_layer_add_layer_gpkg(tmp_path):
     # Prepare test data
     src = test_helper.get_testfile("polygon-parcel")
     dst = tmp_path / "output.gpkg"
@@ -287,6 +287,20 @@ def test_copy_layer_add_layer(tmp_path):
 
     layer2_info = gfo.get_layerinfo(dst, layer2)
     assert layer2_info.featurecount == 48
+
+
+def test_copy_layer_add_layer_shp(tmp_path):
+    # Prepare test data
+    src = test_helper.get_testfile("polygon-parcel")
+    dst = tmp_path / "output.shp"
+    layer1 = gfo.get_default_layer(dst)
+
+    # First "add_layer" to file already while the file doesn't exist yet
+    gfo.copy_layer(src, dst, dst_layer=layer1, write_mode="add_layer")
+
+    # Check result
+    layer1_info = gfo.get_layerinfo(dst, layer1)
+    assert layer1_info.featurecount == 48
 
 
 def test_copy_layer_append_different_layer(tmp_path):
@@ -1164,8 +1178,9 @@ def test_is_geofile_deprecated():
     assert gfo.is_geofile("/test/testje.txt") is False
 
 
-def test_listlayers_errors():
-    path = "not_existing_file.gpkg"
+@pytest.mark.parametrize("suffix", SUFFIXES_FILEOPS)
+def test_listlayers_errors(suffix):
+    path = f"not_existing_file{suffix}"
     with pytest.raises(FileNotFoundError, match=f"File not found: {path}"):
         _ = gfo.listlayers(path)
 
@@ -1939,8 +1954,12 @@ def test_to_file_fid_append_to(tmp_path, engine_setter):
     # Now merge them, but start with the high fid numbers
     gfo.copy(output2_path, output_path)
     gfo.rename_layer(output_path, output_path.stem)
-    gfo.append_to(
-        output1_path, output_path, dst_layer=output_path.stem, preserve_fid=True
+    gfo.copy_layer(
+        output1_path,
+        output_path,
+        dst_layer=output_path.stem,
+        write_mode="append",
+        preserve_fid=True,
     )
 
     # Prepare expected result

tests/test_helper.py
@@ -192,9 +192,9 @@ def get_testfile(
                 tmp_path,
                 src_layer=src_layer,
                 dst_layer=dst_layer,
+                write_mode="add_layer",
                 dst_crs=epsg,
                 reproject=True,
-                append=True,
                 preserve_fid=not explodecollections,
                 dst_dimensions=dimensions,
                 explodecollections=explodecollections,