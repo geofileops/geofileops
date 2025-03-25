b5556539a6bb763ad010d7118d659ffb43e6ec05
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-03-25 23:49:44 +0100

    ENH: add basic support for .gpkg.zip and .shp.zip files in general file/layer operations (#673)
    Following issues need to be solved upstream:
    - [x] For to_file there seems to be an issue in pyogrio, PR:
    https://github.com/geopandas/pyogrio/pull/527
    
    ref #674

--- geofileops/fileops.py
+++ geofileops/fileops.py
@@ -3,21 +3,18 @@
 import enum
 import filecmp
 import logging
+import os
 import pprint
 import shutil
 import string
 import tempfile
 import time
 import warnings
+import zipfile
 from collections.abc import Iterable
 from datetime import date, datetime
 from pathlib import Path
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Literal,
-    Union,
-)
+from typing import Any, Literal, Union
 
 import geopandas as gpd
 import numpy as np
@@ -32,14 +29,12 @@ from pygeoops import GeometryType, PrimitiveType  # noqa: F401
 from geofileops.helpers._configoptions_helper import ConfigOptions
 from geofileops.util import (
     _geofileinfo,
+    _geopath_util,
     _geoseries_util,
     _io_util,
     _ogr_sql_util,
     _ogr_util,
 )
-
-if TYPE_CHECKING:  # pragma: no cover
-    import os
 
 logger = logging.getLogger(__name__)
 
@@ -111,11 +106,10 @@ def listlayers(
         <a href="https://gdal.org/en/stable/user/virtual_file_systems.html" target="_blank">GDAL vsi</a>
 
     """  # noqa: E501
-    path_p = Path(path)
-    if path_p.suffix.lower() == ".shp":
+    if str(path).lower().endswith((".shp", ".shp.zip")):
         if not _vsi_exists(path):
             raise FileNotFoundError(f"File not found: {path}")
-        return [path_p.stem]
+        return [_geopath_util.stem(path)]
 
     datasource = None
     try:
@@ -534,7 +528,7 @@ def get_default_layer(path: Union[str, "os.PathLike[Any]"]) -> str:
     Returns:
         str: The default layer name.
     """
-    return Path(path).stem
+    return _geopath_util.stem(path)
 
 
 def execute_sql(
@@ -609,6 +603,25 @@ def create_spatial_index(
         layer = get_layerinfo(path, layer, raise_on_nogeom=not no_geom_ok)
     if no_geom_ok and layer.geometrycolumn is None:
         return
+    if exist_ok and force_rebuild:
+        raise ValueError("exist_ok and force_rebuild can't both be True")
+
+    # .gpkg.zip files don't support update, so use has_spatial_index up-front to check
+    # if there is an index.
+    if str(path).lower().endswith(".gpkg.zip"):
+        if has_spatial_index(path, layer):
+            if force_rebuild:
+                pass
+            elif exist_ok:
+                return
+            else:
+                raise RuntimeError(
+                    f"spatial index already exists on {path}#{layer.name}"
+                )
+
+        raise RuntimeError(
+            f"create_spatial_index not supported for .gpkg.zip files: {path}"
+        )
 
     # Add index
     path_info = _geofileinfo.get_geofileinfo(path)
@@ -767,7 +780,9 @@ def remove_spatial_index(
 
     # Now really remove index
     try:
-        if path_info.is_spatialite_based:
+        if path_info.is_spatialite_based and not str(path).lower().endswith(
+            ".gpkg.zip"
+        ):
             datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
             result = datasource.ExecuteSQL(
                 "SELECT DisableSpatialIndex("
@@ -2710,6 +2725,39 @@ def copy_layer(
         dst_dimensions=dst_dimensions,
     )
     _ogr_util.vector_translate_by_info(info=translate_info)
+
+
+def _zip(src: Union[str, "os.PathLike[Any]"], dst: Union[str, "os.PathLike[Any]"]):
+    """Zip a file or directory.
+
+    Args:
+        src (PathLike): the file or directory to zip.
+        dst (PathLike): the destination zip file.
+    """
+    # Init input parameters
+    src = Path(src)
+
+    # Zip the file or directory
+    with zipfile.ZipFile(dst, "w") as zipf:
+        if src.is_file():
+            zipf.write(src, src.name)
+        elif src.is_dir():
+            for root, _, files in os.walk(src):
+                for file in files:
+                    file_path = Path(root) / file
+                    zipf.write(file_path, file_path.relative_to(src))
+
+
+def _unzip(src: Union[str, "os.PathLike[Any]"], dst: Union[str, "os.PathLike[Any]"]):
+    """Unzip a zip file.
+
+    Args:
+        src (PathLike): the zip file to unzip.
+        dst (PathLike): the destination directory.
+    """
+    # Unzip the file
+    with zipfile.ZipFile(src, "r") as zipf:
+        zipf.extractall(dst)
 
 
 def _determine_access_mode(
---
+++ tests/test_geopath.py
@@ -0,0 +1,65 @@
+"""Test for functionalities in _geopath_util."""
+
+from pathlib import Path
+
+import pytest
+
+from geofileops.util import _geopath_util
+
+
+@pytest.mark.parametrize(
+    "path, exp_stem",
+    [
+        (Path("/tmp/testje.gpkg"), "testje"),
+        ("/tmp/testje.gpkg.zip", "testje"),
+        ("/tmp/testje.shp", "testje"),
+        ("/tmp/testje.shp.zip", "testje"),
+        ("/tmp/testje.txt", "testje"),
+        ("/tmp/testje", "testje"),
+        ("/tmp/testje.tar.gz", "testje.tar"),
+        ("/tmp/t.estj.e.gpkg", "t.estj.e"),
+        ("/tmp/t.estj.e.gpkg.zip", "t.estj.e"),
+    ],
+)
+def test_stem(path, exp_stem):
+    assert _geopath_util.stem(path) == exp_stem
+
+
+@pytest.mark.parametrize(
+    "path, exp_suffixes",
+    [
+        (Path("/tmp/testje.gpkg"), ".gpkg"),
+        ("/tmp/testje.gpkg.zip", ".gpkg.zip"),
+        ("/tmp/testje.shp", ".shp"),
+        ("/tmp/testje.shp.zip", ".shp.zip"),
+        ("/tmp/testje.txt", ".txt"),
+        ("/tmp/testje", ""),
+        ("/tmp/testje.tar.gz", ".gz"),
+        ("/tmp/t.estj.e.gpkg", ".gpkg"),
+        ("/tmp/t.estj.e.gpkg.zip", ".gpkg.zip"),
+    ],
+)
+def test_suffixes(path, exp_suffixes):
+    assert _geopath_util.suffixes(path) == exp_suffixes
+
+
+@pytest.mark.parametrize(
+    "path, new_stem, exp_path",
+    [
+        ("/tmp/testje.gpkg", "testje_2", "/tmp/testje_2.gpkg"),
+        ("/tmp/testje.gpkg.zip", "testje_2", "/tmp/testje_2.gpkg.zip"),
+        ("/tmp/testje.shp", "testje_2", "/tmp/testje_2.shp"),
+        ("/tmp/testje.shp.zip", "testje_2", "/tmp/testje_2.shp.zip"),
+        ("/tmp/testje.txt", "testje_2", "/tmp/testje_2.txt"),
+        ("/tmp/testje", "testje_2", "/tmp/testje_2"),
+        ("/tmp/testje.tar.gz", "testje_2", "/tmp/testje_2.gz"),
+        ("/tmp/t.estj.e.gpkg", "t.estj.e_2", "/tmp/t.estj.e_2.gpkg"),
+        ("/tmp/t.estj.e.gpkg.zip", "t.estj.e_2", "/tmp/t.estj.e_2.gpkg.zip"),
+    ],
+)
+def test_with_stem(path, new_stem, exp_path):
+    # If input is a string, output should be a string
+    assert _geopath_util.with_stem(path, new_stem) == exp_path
+
+    # If input is a Path, output should be a Path
+    assert _geopath_util.with_stem(Path(path), new_stem) == Path(exp_path)
