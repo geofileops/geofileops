c6ad24d56550a22433aaac59d2215fbd700c4f8e
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-08-21 00:48:39 +0200

    ENH: also write log files to GFO_TMPDIR if specified (#727)

--- geofileops/util/_ogr_util.py
+++ geofileops/util/_ogr_util.py
@@ -14,7 +14,7 @@ from pygeoops import GeometryType
 
 import geofileops as gfo
 from geofileops import _compat, fileops
-from geofileops.util import _geopath_util
+from geofileops.util import _geopath_util, _io_util
 from geofileops.util._general_util import MissingRuntimeDependencyError
 
 # Make sure only one instance per process is running
@@ -478,7 +478,7 @@ def vector_translate(
     # with enable_debug=True nothing is logged. In addition, after
     # gdal.ConfigurePythonLogging is called, the CPL_LOG config setting is ignored.
     if "CPL_LOG" not in config_options:
-        gdal_cpl_log_dir = Path(tempfile.gettempdir()) / "geofileops/gdal_cpl_log"
+        gdal_cpl_log_dir = _io_util.get_tempdir() / "geofileops/gdal_cpl_log"
         gdal_cpl_log_dir.mkdir(parents=True, exist_ok=True)
         fd, gdal_cpl_log = tempfile.mkstemp(suffix=".log", dir=gdal_cpl_log_dir)
         os.close(fd)
