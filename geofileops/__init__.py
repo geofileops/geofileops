c973c74e02bc24417fd46226bd528a886c451397
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-11-28 22:25:36 +0100

    Update __init__.py

--- geofileops/__init__.py
+++ geofileops/__init__.py
@@ -9,6 +9,7 @@ from pathlib import Path
 # USE_PYGEOS to avoid further warnings
 import geopandas._compat as gpd_compat
 import pyogrio
+from osgeo import gdal  # noqa: F401
 
 from geofileops import _compat
 from geofileops.util import _ogr_util
