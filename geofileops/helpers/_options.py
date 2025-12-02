bfbde967058c00d681b9103fb55549cfe5e27fd6
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-12-02 12:38:52 +0100

    Fix merge

--- geofileops/helpers/_options.py
+++ geofileops/helpers/_options.py
@@ -118,8 +118,10 @@ class Options:
     @classproperty
     def get_io_engine(cls) -> str:
         """The IO engine to use."""
-        io_engine = os.environ.get("GFO_IO_ENGINE", default="pyogrio").strip().lower()
-        supported_values = ["pyogrio", "fiona"]
+        io_engine = (
+            os.environ.get("GFO_IO_ENGINE", default="pyogrio-arrow").strip().lower()
+        )
+        supported_values = ["pyogrio", "fiona", "pyogrio-arrow"]
         if io_engine not in supported_values:
             raise ValueError(
                 f"invalid value for configoption <GFO_IO_ENGINE>: '{io_engine}', "
