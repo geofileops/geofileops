2fc594130ad901a9571cc37e9b3d8eced81f7005
Pieter Roggemans <pieter.roggemans@gmail.com>
2025-12-02 13:14:39 +0100

    Try to fix options docs

--- geofileops/helpers/_options_setters.py
+++ geofileops/helpers/_options_setters.py
@@ -5,11 +5,13 @@
 
 from geofileops.helpers._options import Options
 
-copy_layer_sqlite_direct = Options.set_copy_layer_sqlite_direct
-io_engine = Options.set_io_engine
-on_data_error = Options.set_on_data_error
-remove_temp_files = Options.set_remove_temp_files
-subdivide_check_parallel_rows = Options.set_subdivide_check_parallel_rows
-subdivide_check_parallel_fraction = Options.set_subdivide_check_parallel_fraction
-tmp_dir = Options.set_tmp_dir
-worker_type = Options.set_worker_type
+
+class options:
+    copy_layer_sqlite_direct = Options.set_copy_layer_sqlite_direct
+    io_engine = Options.set_io_engine
+    on_data_error = Options.set_on_data_error
+    remove_temp_files = Options.set_remove_temp_files
+    subdivide_check_parallel_rows = Options.set_subdivide_check_parallel_rows
+    subdivide_check_parallel_fraction = Options.set_subdivide_check_parallel_fraction
+    tmp_dir = Options.set_tmp_dir
+    worker_type = Options.set_worker_type
