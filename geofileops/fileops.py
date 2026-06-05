geofileops/geofileops

commit 246e8a3d3464103b9079062444f5be6d41586673

BUG: Add_column function: Check if column was added fails when filetype is sqlite (#818)
resolves #817

geofileops/fileops.py
@@ -1113,15 +1113,6 @@ def add_column(
             datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
             _ogr_util.StartTransaction(datasource)
             datasource.ExecuteSQL(sql_stmt)
-
-            # check if column was really added
-            datasource_layer = datasource.GetLayer(layer)
-            layer_defn = datasource_layer.GetLayerDefn()
-            field_index = layer_defn.GetFieldIndex(name)
-            if field_index == -1:
-                raise RuntimeError(
-                    f"add_column of {name=}, {type=} failed for {path}#{layer}"
-                )
         else:
             logger.warning(f"Column {name} existed already in {path}#{layer}")
 
@@ -1136,6 +1127,16 @@ def add_column(
             datasource.ExecuteSQL(sql_stmt, dialect="SQLITE")
 
         _ogr_util.CommitTransaction(datasource)
+
+        # check if column was really added
+        datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
+        datasource_layer = datasource.GetLayer(layer)
+        layer_defn = datasource_layer.GetLayerDefn()
+        field_index = layer_defn.GetFieldIndex(name)
+        if field_index == -1:
+            raise RuntimeError(
+                f"add_column of {name=}, {type=} failed for {path}#{layer}"
+            )
 
     except Exception as ex:
         _ogr_util.RollbackTransaction(datasource)

tests/general_file_layer_operations/test_geofile.py
@@ -76,13 +76,13 @@ def points_gdf():
     return gdf
 
 
-@pytest.mark.parametrize("suffix", [".gpkg", ".shp"])
+@pytest.mark.parametrize("suffix", [".gpkg", ".shp", ".sqlite"])
 def test_add_column(tmp_path, suffix):
     test_path = test_helper.get_testfile(
         "polygon-parcel", dst_dir=tmp_path, suffix=suffix
     )
-    layer = "parcels" if suffix == ".gpkg" else None
-    geom_column = "geom" if suffix == ".gpkg" else "geometry"
+    layer = "parcels" if suffix in [".gpkg", ".sqlite"] else None
+    geom_column = "geom" if suffix in [".gpkg", ".sqlite"] else "geometry"
 
     # The area column shouldn't be in the test file yet
     layerinfo = gfo.get_layerinfo(path=test_path, layer=layer)
@@ -98,8 +98,10 @@ def test_add_column(tmp_path, suffix):
     assert "AREA" in layerinfo.columns
 
     gdf = gfo.read_file(test_path)
+    # area_col = "area" if suffix == ".sqlite" else "AREA"
+    oppervl_col = "oppervl" if suffix == ".sqlite" else "OPPERVL"
     assert round(gdf["AREA"].astype("float")[0], 1) == round(
-        gdf["OPPERVL"].astype("float")[0], 1
+        gdf[oppervl_col].astype("float")[0], 1
     )
 
     # Add perimeter column
@@ -113,7 +115,7 @@ def test_add_column(tmp_path, suffix):
 
     gdf = gfo.read_file(test_path)
     assert round(gdf["AREA"].astype("float")[0], 1) == round(
-        gdf["OPPERVL"].astype("float")[0], 1
+        gdf[oppervl_col].astype("float")[0], 1
     )
 
     info = gfo.get_layerinfo(test_path)
@@ -123,13 +125,18 @@ def test_add_column(tmp_path, suffix):
     gfo.add_column(test_path, name=existing_column, type="TEXT")
 
     # Force update on an existing column
-    assert gdf["HFDTLT"][0] == "1"
+    hfdtlt_col = "hfdtlt" if suffix == ".sqlite" else "HFDTLT"
+    assert gdf[hfdtlt_col][0] == "1"
     expression = "5"
     gfo.add_column(
-        test_path, name="HFDTLT", type="TEXT", expression=expression, force_update=True
+        test_path,
+        name=hfdtlt_col,
+        type="TEXT",
+        expression=expression,
+        force_update=True,
     )
     gdf = gfo.read_file(test_path)
-    assert gdf["HFDTLT"][0] == "5"
+    assert gdf[hfdtlt_col][0] == "5"
 
 
 @pytest.mark.parametrize(

commit 074856b49d7142ca5121388d4f8f7908aefa38e8

BUG: fix add column for csv files on windows (#823)
Regression introduced in #818: error when adding a column to a csv file
on windows

Additional small changes:
- replace time.perf_counter by datetime.now() for timing because this
gives nicer formatting when printed (hh:mm:ss instead of total seconds)
- also info log if only updating a column

geofileops/fileops.py
@@ -1095,7 +1095,7 @@ def add_column(
 
     type_str = _validate_datatype(type)
 
-    start = time.perf_counter()
+    start = datetime.now()
     layerinfo = get_layerinfo(path, layer, raise_on_nogeom=False)
     layer = layerinfo.name
 
@@ -1104,6 +1104,7 @@ def add_column(
     try:
         # If column doesn't exist yet, create it
         columns_upper = [column.upper() for column in layerinfo.columns]
+        column_added = False
         if name.upper() not in columns_upper:
             logger.info(f"Add column {name} to {path}#{layer}")
             width_str = f"({width})" if width is not None else ""
@@ -1113,6 +1114,7 @@ def add_column(
             datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
             _ogr_util.StartTransaction(datasource)
             datasource.ExecuteSQL(sql_stmt)
+            column_added = True
         else:
             logger.warning(f"Column {name} existed already in {path}#{layer}")
 
@@ -1120,6 +1122,9 @@ def add_column(
         if expression is not None and (
             name not in layerinfo.columns or force_update is True
         ):
+            if not column_added:
+                logger.info(f"Update column {name} on {path}#{layer}")
+
             if datasource is None:
                 datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
                 _ogr_util.StartTransaction(datasource)
@@ -1128,15 +1133,24 @@ def add_column(
 
         _ogr_util.CommitTransaction(datasource)
 
-        # check if column was really added
-        datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_UPDATE)
-        datasource_layer = datasource.GetLayer(layer)
-        layer_defn = datasource_layer.GetLayerDefn()
-        field_index = layer_defn.GetFieldIndex(name)
-        if field_index == -1:
-            raise RuntimeError(
-                f"add_column of {name=}, {type=} failed for {path}#{layer}"
-            )
+        # Verify if column was really correctly added
+        if column_added and datasource is not None:
+            datasource_layer = datasource.GetLayer(layer)
+            layer_defn = datasource_layer.GetLayerDefn()
+            field_index = layer_defn.GetFieldIndex(name)
+            if field_index == -1:
+                # For some file formats, the column is only visible after reopening the
+                # file.
+                datasource_layer = None
+                datasource = None
+                datasource = gdal.OpenEx(str(path), nOpenFlags=gdal.OF_READONLY)
+                datasource_layer = datasource.GetLayer(layer)
+                layer_defn = datasource_layer.GetLayerDefn()
+                field_index = layer_defn.GetFieldIndex(name)
+                if field_index == -1:
+                    raise RuntimeError(
+                        f"add_column of {name=}, {type=} failed for {path}#{layer}"
+                    )
 
     except Exception as ex:
         _ogr_util.RollbackTransaction(datasource)
@@ -1146,9 +1160,9 @@ def add_column(
         datasource = None
 
         # Log time taken if it was slow.
-        took = time.perf_counter() - start
-        if took > 2:  # pragma: no cover
-            logger.info(f"Ready, add_column of {name} took {took:.2f}")
+        took = datetime.now() - start
+        if took.total_seconds() > 2:  # pragma: no cover
+            logger.info(f"Ready, add_column of {name} took {took}")
 
 
 @retry(**RETRY_LOCKED_FILE_KWARGS)  # type: ignore[arg-type]
@@ -1301,7 +1315,7 @@ def add_columns(
             # Add columns in place
             output_tmp_path = path  # type: ignore[assignment]
 
-        start = time.perf_counter()
+        start = datetime.now()
 
         # Get layerinfo and determine `layer` and `output_layer` if not specified
         # Remark: don't reuse the opened datasource here, because then the layer info
@@ -1412,9 +1426,9 @@ def add_columns(
             datasource = None
 
             # Log time taken if it was slow.
-            took = time.perf_counter() - start
-            if took > 2:  # pragma: no cover
-                logger.info(f"Ready, add_columns of {name} took {took:.2f}")
+            took = datetime.now() - start
+            if took.total_seconds() > 2:  # pragma: no cover
+                logger.info(f"Ready, add_columns of {name} took {took}")
 
 
 def _validate_datatype(datatype: str | DataType) -> str:
@@ -1558,7 +1572,7 @@ def update_column(
     # Init
     logger.info(f"Update column {name} in {path}#{layer}")
 
-    start = time.perf_counter()
+    start = datetime.now()
     layerinfo = get_layerinfo(path, layer)
     columns_upper = [column.upper() for column in layerinfo.columns]
     if layerinfo.geometrycolumn is not None:
@@ -1583,9 +1597,9 @@ def update_column(
         datasource = None
 
         # Log time taken if it was slow.
-        took = time.perf_counter() - start
-        if took > 2:  # pragma: no cover
-            logger.info(f"Ready, update_column of {name} took {took:.2f}")
+        took = datetime.now() - start
+        if took.total_seconds() > 2:  # pragma: no cover
+            logger.info(f"Ready, update_column of {name} took {took}")
 
 
 def read_file(

geofileops/util/_geoops_sql.py
@@ -7,7 +7,6 @@ import math
 import os
 import re
 import string
-import time
 import warnings
 from collections.abc import Iterable
 from concurrent import futures
@@ -1455,7 +1454,7 @@ def _has_complex_geoms(path: Path, layer: LayerInfo, max_coords: int) -> bool:
     Returns:
         bool: True if the layer has complex geometries, False otherwise.
     """
-    start = time.perf_counter()
+    start = datetime.now()
 
     sql_template = f"""
         SELECT 1 FROM "{layer.name}" layer
@@ -1528,9 +1527,9 @@ def _has_complex_geoms(path: Path, layer: LayerInfo, max_coords: int) -> bool:
                     pool.shutdown(wait=True, cancel_futures=True)
                     break
 
-    took = time.perf_counter() - start
-    if took > 0:
-        logger.info(f"{complex_found=}, check took {took:.2f}s")
+    took = datetime.now() - start
+    if took.total_seconds() > 0:
+        logger.info(f"{complex_found=}, check took {took}")
 
     return complex_found
 

geofileops/util/_sqlite_util.py
@@ -1,14 +1,13 @@
 """Module containing utilities regarding sqlite/spatialite files."""
 
-import datetime
 import enum
 import logging
 import pprint
 import shutil
 import sqlite3
-import time
 import warnings
 from collections.abc import Iterable
+from datetime import date, datetime
 from pathlib import Path
 from typing import TYPE_CHECKING, Any, Union
 
@@ -405,7 +404,7 @@ def get_columns(
     output_geometrytype: GeometryType | None = None,
 ) -> dict[str, str]:
     # Init
-    start = time.perf_counter()
+    start = datetime.now()
     tmp_dir = None
 
     def get_filetype(path: Path) -> str:
@@ -526,9 +525,9 @@ def get_columns(
                 # cannot be used in "CREATE TABLE".
                 if tmpdata is None:
                     columns[columnname] = "NUMERIC"
-                elif isinstance(tmpdata[column_index], datetime.date):
+                elif isinstance(tmpdata[column_index], date):
                     columns[columnname] = "DATE"
-                elif isinstance(tmpdata[column_index], datetime.datetime):
+                elif isinstance(tmpdata[column_index], datetime):
                     columns[columnname] = "DATETIME"
                 elif isinstance(tmpdata[column_index], str):
                     sql = f'SELECT datetime("{columnname}") FROM tmp;'
@@ -551,9 +550,9 @@ def get_columns(
         if ConfigOptions.get_remove_temp_files and tmp_dir is not None:
             shutil.rmtree(tmp_dir, ignore_errors=True)
 
-    time_taken = time.perf_counter() - start
-    if time_taken > 5:  # pragma: no cover
-        logger.info(f"get_columns ready, took {time_taken:.2f} seconds")
+    took = datetime.now() - start
+    if took.total_seconds() > 5:  # pragma: no cover
+        logger.info(f"get_columns ready, took {took}")
 
     return columns
 